8cc1693767eb46150d569319f47c0ec1033c5dbe
Jun Fritz and Phat Nguyen <jun@>
2017-04-27 14:04:48 -0700

    add conditions, and  error statements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,39 @@
 $(document).ready(function() {
-  $("form#insurance").submit(function(event) {
+  $("form.insurance").submit(function(event) {
     var age = parseInt($("input#age").val());
     var gender = $("select#gender").val();
-//
-    if (age) {
-      var quote = (100 - age) * 3;
+    var haircolor = $("select#haircolor").val();
+    var genre = $("select#genre").val();
 
-      if(gender === "male" && age < 26){
-        quote += 50;
-      }
-      $("#rate").empty().append(quote);
-      $("#quote").show();
-    } else {
-      $("#missing").show();
+
+
+    if (age && gender && haircolor && genre) {
+        if((age >= 0 && age <= 21)  || gender === "male" || (haircolor === "brunette" || "blonde") || (genre === "action" || "romance")){
+        alert("You're match is Mr. Nguyen.");
+        }
+        else if((age >= 22 && age <= 41)  || gender === "male" || (haircolor === "blonde" || "red") || (genre === "action" || "romance")){
+        alert("You're match is Mr. Wise.");
+        }
+        else if((age >= 42 && age <= 61)  || gender === "female" || (haircolor === "red" || "brunette") || (genre === "romance" || "comedy")){
+        alert("You're match is Mrs. Moody.");
+        }
+        else if((age >= 62 && age <= 81)  || gender === "male" || (haircolor === "brunette" || "blonde") || (genre === "action" || "comedy")){
+        alert("You're match is Mr. Fritz.");
+        }
+    }
+    else {
+        if (!age) {
+          $("#missing").show();
+        }
+        if (!gender) {
+          $("#missing-gender").show();
+        }
+        if (!haircolor) {
+          $("#missing-color").show();
+        }
+        if (!genre) {
+          $("#missing-genre").show();
+        }
     }
 
     event.preventDefault();
